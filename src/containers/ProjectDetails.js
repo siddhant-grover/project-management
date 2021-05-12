ff5d3de053bcd6892f06dac401dbefdcb71f00fb
Siddhant Grover <siddhantgroverboss@gmail.com>
2021-05-12 21:24:41 +0530

    Project Details fixed

--- src/containers/ProjectDetails.js
+++ src/containers/ProjectDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router';
 
 import firebase from "../config/fbConfig"
@@ -6,30 +6,36 @@ function ProjectDetails(props) {
     
     let id = useRef(useParams().id);
    
-    let item = useRef({})
-   useEffect(()=>{
-    item.current =  firebase.firestore().collection('Projects').doc(id.current).get()
-        .then(snapshot=>snapshot.data())
-   },[])
-            
-   
-   
-  
-    return (
-      
- 
-        <div>
-            <h2>{item.current.title} id:{id.current}</h2>
-            <p>Author:{item.current.authorLastName} {item.current.authorFirstName}</p>
-            <p>{item.current.content}</p>
-            <p>Date : 24 july 2021</p>
+    let [item,setItem] = useState({})
 
-        </div>
+   useEffect(()=>{
+  
+        firebase.firestore().collection('Projects').doc(id.current).get()
+        .then(snapshot=>setItem(snapshot.data())).catch(error=>{throw error })
+     
+   
         
-       
-        
-        
-    );
+   },[])
+   console.log("d")
+    console.log(item)
+    console.log(id)
+    if(!(Object.keys(item).length===0)){
+        return(
+                <div>
+                    <h2>{item.title} id:{id.current}</h2>
+                    <p>Author:{item.authorLastName} {item.authorFirstName}</p>
+                    <p>{item.content}</p>
+                    <p>Date : 24 july 2021</p>
+
+                </div>
+        )
+    }
+    else{
+        return (
+        <p>Loading.....</p>
+        )
+    }
+
 }
 
     
